798820966d31a7f5df0d09e14968c57dea696269
leo_song <2192383945@qq.com>
2023-05-30 14:39:13 +0800

    修复文件上传功能

--- src/renderer/util/fileSystem.js
+++ src/renderer/util/fileSystem.js
@@ -154,7 +154,8 @@ export const uploadImage = async (pathname, image, preferences) => {
     if (typeof filepath !== 'string') {
       isPath = false
       const data = new Uint8Array(filepath)
-      filepath = path.join(tmpdir(), +new Date())
+      filepath = path.join(tmpdir(), `${new Date().getTime()}.png`)
+      console.log(filepath, data)
       await fs.writeFile(filepath, data)
     }
     if (uploader === 'picgo') {
